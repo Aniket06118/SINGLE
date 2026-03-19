bb5436796844b73f192366d09322e9d2972e3124
Prachee Mahapatra <pracheemahapatra2006@gmail.com>
2026-03-19 18:56:32 +0530

    Added the CFG snippet

--- sampling.py
+++ sampling.py
@@ -7,6 +7,8 @@ import os
 
 from torchvision.utils import save_image
 
+from text_encoder import text_emb, unconditional_emb
+
 class DDPM_sampler(nn.Module):
 
     def __init__(self, diffusion):
@@ -19,7 +21,7 @@ class DDPM_sampler(nn.Module):
         self.device = diffusion.beta.device
 
     @torch.no_grad()
-    def p_sample(self, model, x, t):
+    def p_sample(self, model, x, t, cond_emb, uncond_emb, cfg_scale):
 
         batch_size = x.shape[0]
         t_tensor = torch.full((batch_size,), t, dtype=torch.long, device=self.device)
@@ -31,8 +33,11 @@ class DDPM_sampler(nn.Module):
         sqrt_recip_alpha = torch.sqrt(1.0 / alpha_t)
         sqrt_one_minus_alpha_bar = torch.sqrt(1 - alpha_bar_t)
 
+        eps_uncond = model(x, t_tensor, uncond_emb)
+        eps_cond   = model(x, t_tensor, cond_emb)
+
         #Predict noise
-        eps_theta = model(x, t_tensor)
+        eps_theta = eps_uncond + cfg_scale * (eps_cond - eps_uncond)
 
         #Reverse mean
         model_mean = sqrt_recip_alpha*(x - (beta_t/sqrt_one_minus_alpha_bar)*eps_theta)
@@ -44,13 +49,18 @@ class DDPM_sampler(nn.Module):
             return model_mean
 
     @torch.no_grad()
-    def sample(self, model, shape):
+    def sample(self, model, shape, prompts, cfg_scale=7.5):
 
         device = next(model.parameters()).device
         x = torch.randn(shape, device=device)
 
+        B = shape[0]
+
+        cond_emb = text_emb(prompts).to(device)
+        uncond_emb = unconditional_emb(B).to(device)
+
         for t in tqdm(reversed(range(self.timesteps))):
-            x = self.p_sample(model, x, t)
+            x = self.p_sample(model, x, t, cond_emb, uncond_emb, cfg_scale)
 
         return x
 
@@ -72,7 +82,10 @@ def main():
     model.to(device)
     model.eval()
 
-    samples = sampler.sample(model, (1, 3, 256, 256))
+    prompts = ["a photo of a dog"]
+    #prompt given for example. Change as needed
+
+    samples = sampler.sample(model,(1, 3, 256, 256),prompts,cfg_scale=7.5)
     samples = samples.clamp(-1, 1)
     samples = (samples+1)/2
 
